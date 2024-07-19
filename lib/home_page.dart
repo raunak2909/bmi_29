3cb1c85cb5b3f6940cf7e558f2eab6fbb16d140e
raunakpurohit <raunak.wscubetech@gmail.com>
2024-07-19 19:35:56 +0530

    feature: added splash page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<StatefulWidget>{
+  /// my controllers
   TextEditingController wtController = TextEditingController();
   TextEditingController ftController = TextEditingController();
   TextEditingController inController = TextEditingController();
